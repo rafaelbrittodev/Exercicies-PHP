576fec506f4f336e8cb05689e45e31b06c42bfae
Rafael Brito <142283293+rafaelbrittodev@users.noreply.github.com>
2024-03-16 22:24:36 -0300

    <update>reorganize-project

--- class-27-02/index.php
+++ class-27-02/index.php
@@ -17,36 +17,34 @@ $sobrenome = "da Silva rito";
 $idade = 25;
 
 # tipos de print
-print '$nome'. "<br>"; # nome das variaveis
-print '$sobrenome'. "<br>";
-print '$idade'. "<br>" . "<br>";
+print '$nome' . "<br>"; # nome das variaveis
+print '$sobrenome' . "<br>";
+print '$idade' . "<br>" . "<br>";
 
-print $nome. "<br>"; # dados das variaveis
-print $sobrenome. "<br>";
-print $idade. "<br>" . "<br>";
+print $nome . "<br>"; # dados das variaveis
+print $sobrenome . "<br>";
+print $idade . "<br>" . "<br>";
 
 
-print_r ($nome . "<br>"); # informacao da variavel
-print_r ($sobrenome . "<br>");
-print_r ($idade . "<br>" . "<br>");
+print_r($nome . "<br>"); # informacao da variavel
+print_r($sobrenome . "<br>");
+print_r($idade . "<br>" . "<br>");
 
 
 echo "$nome <br>"; # echo é o print mais robusto para concatenacao
 echo "$sobrenome <br>";
 echo "$idade <br>";
 
-var_dump ('$nome'); # qt caracteres name variavel
-var_dump ('$sobrenome');
-var_dump ('$idade');
+var_dump('$nome'); # qt caracteres name variavel
+var_dump('$sobrenome');
+var_dump('$idade');
 
-var_dump ("$nome"); # qt caracteres dado variavel
-var_dump ("$sobrenome");
-var_dump ("$idade"); 
+var_dump("$nome"); # qt caracteres dado variavel
+var_dump("$sobrenome");
+var_dump("$idade");
 
-$a = readline ("Teste: ") # input que não funciona em .php off-line
+$a = readline("Teste: "); # input que não funciona em .php off-line
 
 # 000webhost.com - cadastrar email para entregar atividades para o professor
 # infinityfreeapp.com - cadastrar email para entregar atividades para o professor
 # USBWebServer.v.8.6.2 - caso o local server wamp não funcione
-
-?>
